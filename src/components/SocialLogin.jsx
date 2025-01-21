8424cfe277c24048eef735af7de5093b9246608a
mahmud-bhuiyan <bh.mahmud@gmail.com>
2025-01-22 02:29:07 +0600

    custom button design updated

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -5,6 +5,7 @@ import { toast } from "react-toastify";
 import { AuthContext } from "../context/AuthContextProvider";
 import { signInWithGoogle } from "../services/apis/User";
 import handleError from "../utils/handleError";
+import CustomButton from "./Custom/CustomButton";
 
 const SocialLogin = () => {
   const { googleSignIn } = useContext(AuthContext);
@@ -40,15 +41,14 @@ const SocialLogin = () => {
 
   return (
     <div className="flex flex-col items-center">
-      <button
-        onClick={handleGoogleSignIn}
-        disabled={loading}
-        className="flex items-center justify-center px-6 py-2 border border-transparent text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 shadow-sm transition duration-150 ease-in-out"
+      <CustomButton
         type="button"
-      >
-        <FcGoogle className="w-5 h-5 mr-2" />
-        <span>{loading ? "Signing In..." : "Sign In with Google"}</span>
-      </button>
+        onClick={handleGoogleSignIn}
+        loading={loading}
+        buttonText="Sign In with Google"
+        loadingText="Signing In..."
+        icon={FcGoogle}
+      />
     </div>
   );
 };
